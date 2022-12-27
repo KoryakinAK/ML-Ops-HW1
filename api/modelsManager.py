37134c69723eb5b29d82295c470473dad3bb5d01
Koryakin Alexey <koryakin-ak@yandex-team.ru>
2022-12-27 06:58:22 +0300

    models storage manager minor fixes

--- api/modelsManager.py
+++ api/modelsManager.py
@@ -1,9 +1,16 @@
 import os
 import pickle
 
+availableClasses = {
+    "dummyClassifier": ["strategy", "constant"],
+    "dummyRegressor": ["strategy"]
+}
+
 class ModelsManager:
-    def init(self, models_folder):
-        self.models_folder = models_folder
+    models = {}
+
+    def init(self):
+        self.models_folder = "storage"
         if not os.path.exists(self.models_folder):
             os.makedirs(self.models_folder)
         self.models = {}
@@ -18,9 +25,13 @@ class ModelsManager:
         model_path = os.path.join(self.models_folder, f"{name}.pkl")
         with open(model_path, "rb") as f:
             model = pickle.load(f)
+        loadedModels[name] = model
         return model
     
     def load_all(self):
         for model_name in os.listdir(self.models_folder):
             model_name = model_name.split(".")[0]
             self.load(model_name)
+    
+    def getTrainedModels(self):
+        return list(self.models.keys())
